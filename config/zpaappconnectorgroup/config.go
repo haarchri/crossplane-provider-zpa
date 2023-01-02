da3276d0505e758e83ef2f353e38df9375fe40e2
GitHub Terraform <zscaler-github-P@zscaler.com>
2023-01-02 15:18:27 -0800

    Added several resources

---
+++ config/zpaappconnectorgroup/config.go
@@ -0,0 +1,10 @@
+package zpaappconnectorgroup
+
+import "github.com/upbound/upjet/pkg/config"
+
+// Configure configures individual resources by adding custom ResourceConfigurators.
+func Configure(p *config.Provider) {
+	p.AddResourceConfigurator("zpa_app_connector_group", func(r *config.Resource) {
+		r.ShortGroup = "appconnectorgroup"
+	})
+}
---
+++ config/zpaserviceedgegroup/config.go
@@ -0,0 +1,10 @@
+package zpaserviceedgegroup
+
+import "github.com/upbound/upjet/pkg/config"
+
+// Configure configures individual resources by adding custom ResourceConfigurators.
+func Configure(p *config.Provider) {
+	p.AddResourceConfigurator("zpa_service_edge_group", func(r *config.Resource) {
+		r.ShortGroup = "serviceedgegroup"
+	})
+}
